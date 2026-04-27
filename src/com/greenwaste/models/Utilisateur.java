c6c58840bee72d9da9b0d2b006c5694fe54f0ecb
raghedchemakh-afk <raghedchemakh@gmail.com>
2026-04-27 23:16:52 +0100

    Sprint 2 - Logique metier Utilisateur et Authentification

--- src/com/greenwaste/models/Utilisateur.java
+++ src/com/greenwaste/models/Utilisateur.java
@@ -1,22 +1,46 @@
+
 public class Utilisateur {
 
-    private String nom;
-    private String prenom;
-    private String email;
+    private int     id;
+    private String  nom;
+    private String  email;
+    private String  motDePasseHash;
+    private String  role;
+    private boolean compteActif;
 
-    public Utilisateur(String nom,
-                       String prenom,
-                       String email) {
+    public Utilisateur() {}
 
-        this.nom = nom;
-        this.prenom = prenom;
-        this.email = email;
+    public Utilisateur(int id, String nom,
+                       String email, String role) {
+        this.id          = id;
+        this.nom         = nom;
+        this.email       = email;
+        this.role        = role;
+        this.compteActif = false;
     }
 
-    public void afficherUtilisateur() {
+    public String hasherMotDePasse(String mdp) {
+        return String.valueOf(mdp.hashCode());
+    }
 
-        System.out.println("Nom : " + nom);
-        System.out.println("Prenom : " + prenom);
-        System.out.println("Email : " + email);
+    public int     getId()             { return id; }
+    public String  getNom()            { return nom; }
+    public String  getEmail()          { return email; }
+    public String  getRole()           { return role; }
+    public boolean isCompteActif()     { return compteActif; }
+    public String  getMotDePasseHash() { return motDePasseHash; }
+
+    public void setId(int id)                  { this.id = id; }
+    public void setNom(String nom)             { this.nom = nom; }
+    public void setEmail(String email)         { this.email = email; }
+    public void setRole(String role)           { this.role = role; }
+    public void setCompteActif(boolean b)      { this.compteActif = b; }
+    public void setMotDePasseHash(String hash) { this.motDePasseHash = hash; }
+
+    @Override
+    public String toString() {
+        return "Utilisateur[" + id + "] "
+            + nom + " | " + role
+            + " | Actif: " + compteActif;
     }
 }
