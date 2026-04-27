81fd31e2c53f875ad6957099e3af801ea5fe3cd2
raghedchemakh-afk <raghedchemakh@gmail.com>
2026-04-27 22:40:43 +0100

    Ajout Utilisateur et Authentification

--- src/com/greenwaste/models/Utilisateur.java
+++ src/com/greenwaste/models/Utilisateur.java
@@ -1,46 +1,22 @@
-
 public class Utilisateur {
 
-    private int     id;
-    private String  nom;
-    private String  email;
-    private String  motDePasseHash;
-    private String  role;
-    private boolean compteActif;
+    private String nom;
+    private String prenom;
+    private String email;
 
-    public Utilisateur() {}
+    public Utilisateur(String nom,
+                       String prenom,
+                       String email) {
 
-    public Utilisateur(int id, String nom,
-                       String email, String role) {
-        this.id          = id;
-        this.nom         = nom;
-        this.email       = email;
-        this.role        = role;
-        this.compteActif = false;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
     }
 
-    public String hasherMotDePasse(String mdp) {
-        return String.valueOf(mdp.hashCode());
-    }
+    public void afficherUtilisateur() {
 
-    public int     getId()             { return id; }
-    public String  getNom()            { return nom; }
-    public String  getEmail()          { return email; }
-    public String  getRole()           { return role; }
-    public boolean isCompteActif()     { return compteActif; }
-    public String  getMotDePasseHash() { return motDePasseHash; }
-
-    public void setId(int id)                  { this.id = id; }
-    public void setNom(String nom)             { this.nom = nom; }
-    public void setEmail(String email)         { this.email = email; }
-    public void setRole(String role)           { this.role = role; }
-    public void setCompteActif(boolean b)      { this.compteActif = b; }
-    public void setMotDePasseHash(String hash) { this.motDePasseHash = hash; }
-
-    @Override
-    public String toString() {
-        return "Utilisateur[" + id + "] "
-            + nom + " | " + role
-            + " | Actif: " + compteActif;
+        System.out.println("Nom : " + nom);
+        System.out.println("Prenom : " + prenom);
+        System.out.println("Email : " + email);
     }
 }
